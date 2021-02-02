779825c627f476c420b7836056e5a7ea71921984
Hunter Evanoff <hunter547@gmail.com>
2021-02-01 17:03:00 -0700

    Group Project 1 - solo attempt before working with team on ideal implementation.

---
+++ project1/hunter-output.md
@@ -0,0 +1,15 @@
+```
+$ python3 hunter.py
+Please enter a person's name: Brian
+Please enter the name of a college: the Community College of Philadelphia
+Please enter the name of a company or business: Enron
+Please enter a job or occupation: accountant
+Please enter a foreign city: Tokyo
+Please enter a restaurant: Rainforest Cafe
+Please enter an activity: scuba diving
+Please enter another activity: paragliding
+Please enter an animal: emu
+Please enter another name: Dave
+Please enter a retail store: Blockbuster
+There once lived a person named Brian who just graduated from the Community College of Philadelphia. After accepting a job with Enron as a(n) accountant, Brian decided to first take a vacation to Tokyo. Upon arriving in Tokyo, Brian planned to first visit Rainforest Cafe before enjoying an evening of scuba diving and paragliding. The next day, Brian adopted a(n) emu named Dave found in front of an abandoned Blockbuster. Brian and Dave lived happily ever after.
+```
---
+++ project1/hunter.py
@@ -0,0 +1,15 @@
+# Collect all inputs necessary to create the madlib.
+name = input("Please enter a person\'s name: ")
+college = input("Please enter the name of a college: ")
+company = input("Please enter the name of a company or business: ")
+job = input("Please enter a job or occupation: ")
+city = input("Please enter a foreign city: ")
+restaurant = input("Please enter a restaurant: ")
+activity = input("Please enter an activity: ")
+anotherActivity = input("Please enter another activity: ")
+animal = input("Please enter an animal: ")
+anotherName = input("Please enter another name: ")
+store = input("Please enter a retail store: ")
+
+# Display the paragraph with all the inputs in the console
+print(f"There once lived a person named {name} who just graduated from {college}. After accepting a job with {company} as a(n) {job}, {name} decided to first take a vacation to {city}. Upon arriving in {city}, {name} planned to first visit {restaurant} before enjoying an evening of {activity} and {anotherActivity}. The next day, {name} adopted a(n) {animal} named {anotherName} found in front of an abandoned {store}. {name} and {anotherName} lived happily ever after.")
