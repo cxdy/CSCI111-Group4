122bc877cb3cc1241dc6aeeab844eb036521a565
bhaines6 <75000740+bhaines6@users.noreply.github.com>
2021-02-03 13:41:47 -0500

    Add files via upload

---
+++ project1/GroupProect1 BH.py
@@ -0,0 +1,14 @@
+firstname1 = input("Enter a first name: ")
+college = input("Enter the name of a college: ")
+business = input("Enter the name of a business: ")
+job = input("Enter a job: ")
+city = input("Enter the name of a city: ")
+restaurant = input("Enter the name of a restaurant: ")
+activity1 = input("Enter an activity: ")
+activity2 = input("Enter another activity: ")
+animal = input("Enter a type of animal: ")
+firstname2 = input("Enter a different first name: ")
+store = input("Enter the name of a store: ")
+
+# Display the paragraph with all the inputs in the console
+print(f"There once lived a person named {firstname1} who recently graduated from {college}. After accepting a job with {business} as a(n) {job}, {firstname1} decided to first take a vacation to {city}. Upon arriving in {city}, {firstname1} planned to visit {restaurant} before enjoying an evening of {activity1} and {activity2}. The next day, {firstname1} adopted a(n) {animal} named {firstname2} found in front of an abandoned {store}. {firstname1} and {firstname2} lived happily ever after .")
