4bf3dd64a5f9e1d38af157a110a942bc1ffcda53
bhaines6 <75000740+bhaines6@users.noreply.github.com>
2021-02-03 13:42:55 -0500

    Add files via upload

---
+++ project1/GroiupProject 1 Output.py
@@ -0,0 +1,18 @@
+Python 3.9.1 (v3.9.1:1e5d33e9b9, Dec  7 2020, 12:10:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: /Users/Haines/Documents/CSCI/GroupProect1 BH.py ===========
+Enter a first name: George
+Enter the name of a college: Temple University
+Enter the name of a business: KPMG
+Enter a job: IT Specialist
+Enter the name of a city: Myrtle Beach
+Enter the name of a restaurant: Chick-Fil-A
+Enter an activity: golfing
+Enter another activity: drinking
+Enter a type of animal: perrot
+Enter a different first name: Harry
+Enter the name of a store: Best Buy
+There once lived a person named George who recently graduated from Temple University. After accepting a job with KPMG as a(n) IT Specialist, George decided to first take a vacation to Myrtle Beach. Upon arriving in Myrtle Beach, George planned to visit Chick-Fil-A before enjoying an evening of golfing and drinking. The next day, George adopted a(n) perrot named Harry found in front of an abandoned Best Buy. George and Harry lived happily ever after .
+>>> 
