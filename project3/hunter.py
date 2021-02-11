e76d47ee15b056b5a411e8f32145fa74a2934ba6
Hunter Evanoff <hunter547@gmail.com>
2021-02-11 13:03:18 -0700

    First draft on Group Project 3 before we hop on the discord call tonight.

---
+++ project3/hunter.py
@@ -0,0 +1,41 @@
+# Ask user to input a month in the number format and year above 1000.
+month = int(input("Enter a month (1-12): "))
+year = int(input("Enter a year (>1000): "))
+
+# Set up var that will be assigned later on to represent the number of days in a particular month.
+number_of_days = ""
+
+# Start an if/elif chain that will determine the number of days in the month provided.
+if (month == 1) :
+  number_of_days = 31
+elif (month == 2) : 
+  # When month equals 2, if the year provided has no remainder when both divided by 100 and 400 
+  # or if the year has a remainder when divided by 100, but not when divided by 4, then assign 29 days.
+  # Otherwise assign 28 days when month equals 2.
+  if((year % 100 == 0 and year % 400 == 0) or (year % 100 != 0 and year % 4 == 0)):
+    number_of_days = 29
+  else:
+    number_of_days = 28
+elif (month == 3) :
+  number_of_days = 31
+elif (month == 4) :
+  number_of_days = 30
+elif (month == 5) :
+  number_of_days = 31
+elif (month == 6) : 
+  number_of_days = 30
+elif (month == 7) :
+  number_of_days = 31
+elif (month == 8) :
+  number_of_days = 31
+elif (month == 9) :
+  number_of_days = 30
+elif (month == 10) :
+  number_of_days = 31
+elif (month == 11) :
+  number_of_days = 30
+elif (month == 12) :
+  number_of_days = 31
+
+# Print the number of days with the month and year provided.
+print(f"There are {number_of_days} days in {month}/{year}")
