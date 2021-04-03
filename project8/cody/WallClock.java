b84351e6788f12ee4ace1d78932a04f1817ec56b
Cody Kaczynski <cxdy@users.noreply.github.com>
2021-04-03 14:02:58 -0400

    one light goes out they all go out

--- project8/cody/WallClock.java
+++ project8/cody/WallClock.java
@@ -30,117 +30,89 @@ public class WallClock {
         // Second 59 -> 60 does not roll over the minute, just the second
         // I would assume 1:59:59 when adding a second would not go to the next hour either
 
-        int hrs = i / 3600;
-        int extraHrs = i - hrs * 3600;
-        int mins = extraHrs / 60;
-        int secs = extraHrs - mins * 60;
+        int secs = i % 60;
+        int hrs = i / 60;
+        int mins = hours % 60;
 
-        // if the hours are less than 23, set it
-        if (hrs <= 23) {
-            hours = hrs;
-        }
-
-        if (mins <= 59) {
-            minutes = mins;
-        }
-
-        if (secs <= 59) {
-            seconds = secs;
-        }
+        hours = hrs / 60;
+        minutes = mins;
+        seconds = secs;
 
     }
 
-    public void Forward(int i) {
-        // save old values
+    public void Forward(int i){
         int oldHours = hours;
         int oldMinutes = minutes;
         int oldSeconds = seconds;
 
-        // convert i (seconds) to hh:mm:ss format
         Convert(i);
 
-        // init comparison variables
-        int fwdHrs;
-        int fwdMins;
-        int fwdSecs;
+        int fixSeconds = 0;
+        int fixMinutes = 0;
+        int fixHours = 0;
 
-        // if the sum of the oldHrs and new hrs is less than or equal to 12, add together
-        // this math is all wrong, but the script kind of works
-        if (oldHours + hours < 12) {
-            fwdHrs = oldHours + hours;
+        if (seconds + oldSeconds > 59){
+            fixSeconds = (seconds + oldSeconds) - 59;
+            fixMinutes += 1;
         } else {
-            fwdHrs = (oldHours + hours) - 12;
+            fixSeconds += seconds + oldSeconds;
         }
 
-        if (oldMinutes + minutes <= 59) {
-            fwdMins = oldMinutes + minutes;
+        if (minutes + oldMinutes > 59) {
+            fixMinutes = (minutes + oldMinutes) - 59;
+            fixHours += 1;
         } else {
-            fwdMins = (oldMinutes + minutes) - 59;
+            fixMinutes += minutes + oldMinutes;
         }
 
-        if (oldSeconds + seconds <= 59) {
-            fwdSecs = oldSeconds + seconds;
+        if (hours + oldHours > 23) {
+            fixHours += (hours + oldHours) - 23;
         } else {
-            fwdSecs = (oldSeconds + seconds) - 59;
+            fixHours += hours + oldHours;
         }
 
-        hours = fwdHrs;
-        minutes = fwdMins;
-        seconds = fwdSecs;
+        hours = fixHours;
+        minutes = fixMinutes;
+        seconds = fixSeconds;
 
     }
+
 
     public void Reverse(int i) {
         // Reverse class
-
-        // Weird bug
-        // TIME> forward 60
-        // The new time is: 23:59:00
-        // TIME> forward 60
-        // The new time is: 23:01:00
-        // TIME> forward 3540
-        // The new time is: 23:01:00
-        // TIME> reverse 3600
-        // The new time is: 45:01:00
-
-        // keep the old (current) time
         int oldHours = hours;
         int oldMinutes = minutes;
         int oldSeconds = seconds;
 
-        // convert the time in seconds to hr:mm:ss
         Convert(i);
 
-        // initialize the comparison variables
-        int revHrs;
-        int revMins;
-        int revSecs;
+        int fixSeconds = 0;
+        int fixMinutes = 0;
+        int fixHours = 0;
 
-        // if the old and new together are less than or equal to 24, safely subtract
-        if (oldHours - hours >= 24) {
-            revHrs = oldHours - hours;
+        if (seconds - oldSeconds > 59){
+            fixSeconds = (seconds - oldSeconds) - 59;
+            fixMinutes -= 1;
         } else {
-            // i think this is where my bug is
-            revHrs = (oldHours - hours) + 23;
+            fixSeconds -= seconds - oldSeconds;
         }
 
-        // same as above, but greater than or equal to
-        if (oldMinutes - minutes <= 60) {
-            revMins = oldMinutes - minutes;
+        if (minutes - oldMinutes > 59) {
+            fixMinutes = (minutes - oldMinutes) - 59;
+            fixHours -= 1;
         } else {
-            revMins = (oldMinutes - minutes) + 59;
+            fixMinutes -= minutes - oldMinutes;
         }
 
-        if (oldSeconds - seconds <= 60) {
-            revSecs = oldSeconds - seconds;
+        if (hours - oldHours > 23) {
+            fixHours -= (hours - oldHours) - 23;
         } else {
-            revSecs = (oldSeconds - seconds) + 59;
+            fixHours -= hours - oldHours;
         }
 
-        // set the new time
-        hours = revHrs;
-        minutes = revMins;
-        seconds = revSecs;
+        hours = fixHours;
+        minutes = fixMinutes;
+        seconds = fixSeconds;
 
     }
 
