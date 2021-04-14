1caf05b3fea18b62f08f2bb60217cf07ff5f7ca5
bhaines6 <75000740+bhaines6@users.noreply.github.com>
2021-04-14 18:58:39 -0400

    Add files via upload

--- project9/ComboLock.java
+++ project9/ComboLock.java
@@ -1,45 +1,45 @@
-package csci111.group4;
-
 public class ComboLock {
+    //private fields to build comboLock calculations
+    private final int code1;
+    private final int code2;
+    private final int code3;
+    private int code1Input;
+    private int code2Input;
+    private int code3Input;
+    private int showDial = 0;
 
-    int[] combo = {0, 0, 0};
-    int[] attempt = {0, 0, 0};
 
-    public ComboLock(int one, int two, int three){
-        if (one < 0) { combo[0] = 0; } else if(one > 39) { combo[0] = 39; } else { combo[0] = one; }
-        if (two < 0) { combo[1] = 0; } else if(two > 39) { combo[1] = 39; } else { combo[1] = two; }
-        if (three < 0) { combo[2] = 0; } else if(three > 39) { combo[2] = 39; } else { combo[2] = three; }
+    //Public constructor setting parameter limits of the input between 0 and 39 for each of the three inputs.
+    public ComboLock(int left1, int right, int left2) {
+        code1 = left1 < 39 & left1 > 0 ? left1 : left1 < 0 ? 0 : 39;
+        code2 = right < 39 & right > 0 ? right : right < 0 ? 0 : 39;
+        code3 = left2 < 39 & left2 > 0 ? left2 : left2 < 0 ? 0 : 39;
     }
-
+    //reset method that reverts all combo inputs to 0
     public void reset() {
-        attempt[0] = 0;
-        attempt[1] = 0;
-        attempt[2] = 0;
+        code1Input = 0;
+        code2Input = 0;
+        code3Input = 0;
+        showDial = 0;
+    }
+    //a method that uses an integer input to move the dial "clockwise" or forward the desired amount input by the user.
+    public void turnClockwise(int clockwiseAmount) {
+        showDial = Math.abs(showDial - (clockwiseAmount % 40));
+        code2Input = showDial;
+
+    }
+    //a method that uses an integer input to move the dial "counter clockwise" or backward the desired amount input by the user.
+    public void turnCounterClockwise(int counterClockwiseAmount) {
+        showDial = (showDial + counterClockwiseAmount) % 40;
+        if (code2Input == 0) { code1Input = showDial; }
+        else { code3Input = showDial; }
+
     }
 
-    public int turnClockwise(int turn, int position) {
-        if (turn + attempt[position] > 39) {
-            attempt[position] = turn % 39;
-        } else if (turn <= 0) {
-            attempt[position] = 0;
-        }
-        return attempt[position];
-    }
-
-    public int turnCounterClockwise(int turn, int position) {
-        if (turn + attempt[position] > 39) {
-            attempt[position] = turn % 39;
-        } else if (turn <= 0) {
-            attempt[position] = 0;
-        }
-        return attempt[position];
-    }
-
-    public boolean open(){
-        if(combo[0] == attempt[0] && combo[1] == attempt[1] && combo[2] == attempt[2]) {
-            return true;
-        } else {
-            return false;
-        }
+    //method to open lock if correct inputs are given the lock will return true, if incorrect inputs are given, a false output appears.
+    public boolean open() {
+        return code1Input == code1 &
+                code2Input == code2 &
+                code3Input == code3;
     }
 }
---
+++ project9/ComboLockDemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ComboLockDemo {
+
+    import java.util.Scanner;
+
+    public static void main(String[] args) {
+        ComboLock comboLock;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Code 1: ");
+        comboLock.turnCounterClockwise();
+        System.out.print("Enter Code 2: ");
+        comboLock.turnClockwise();
+        System.out.print("Enter Code 3: ");
+        comboLock.turnCounterClockwise();}
+
+
+        boolean open = ComboLock.open();
+
+        if (open) { System.out.println("Success");
+            break;
+        }
+        else { System.out.println("Fail");}
+        if else{
+            ComboLock.reset();}
+
+
+    }
+}
