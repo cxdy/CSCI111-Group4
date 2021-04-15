89fa07f22fb083434c16751378ec77c2868ba41e
Cody Kaczynski <cxdy@users.noreply.github.com>
2021-04-14 20:10:06 -0400

    Update ComboLockDemo.java

--- project9/ComboLockDemo.java
+++ project9/ComboLockDemo.java
@@ -1,30 +1,46 @@
+package csci111.group4;
+
 import java.util.Scanner;
 
 public class ComboLockDemo {
 
-    import java.util.Scanner;
-
     public static void main(String[] args) {
-        ComboLock comboLock;
+
+        // init scanner for keyboard input
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Code 1: ");
-        comboLock.turnCounterClockwise();
-        System.out.print("Enter Code 2: ");
-        comboLock.turnClockwise();
-        System.out.print("Enter Code 3: ");
-        comboLock.turnCounterClockwise();}
+        // set combo by constructing new combolock object
+        ComboLock Combo = new ComboLock(26, 12, 33);
 
+        // vars to store each turn
+        int turn1;
+        int turn2;
+        int turn3;
 
-        boolean open = ComboLock.open();
+        // loop for endless retries
+        while (true) {
+            // grab input for first turn
+            System.out.print("Enter turn #1: ");
+            turn1 = input.nextInt();
+            // call function turnCounterClockwise
+            Combo.turnCounterClockwise(turn1);
+            // second turn
+            System.out.print("Enter turn #2: ");
+            turn2 = input.nextInt();
+            // this time go clockwise
+            Combo.turnClockwise(turn2);
+            System.out.print("Enter turn #3: ");
+            turn3 = input.nextInt();
+            Combo.turnCounterClockwise(turn3);
 
-        if (open) { System.out.println("Success");
-            break;
+            // attempt to open, true means unlocked
+            System.out.println(Combo.open());
+            // reset the attempt
+            Combo.reset();
+
         }
-        else { System.out.println("Fail");}
-        if else{
-            ComboLock.reset();}
-
 
     }
+
 }
+
